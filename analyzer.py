62166c3e6230ddb92a62c0439f8aad26a3bc822a
noormichael <nsmichael31@gmail.com>
2017-02-25 14:34:02 -0600

    init

--- analyzer.py
+++ analyzer.py
@@ -1,2 +1,8 @@
-from nltk import *
+#!/usr/bin/env python
 
+from nltk import *
+import xml.etree.ElementTree as ET
+
+root = ET.parse('sms_corpus.xml').getroot()
+
+messages = [m.find('text') for m in root]
